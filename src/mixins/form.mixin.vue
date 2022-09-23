2b7d68500d55bfed8ca7e40201754f14b0cdfd14
Yan Vidal <yan.vidal@vitta.me>
2022-09-23 20:59:09 -0300

    feat: insert mixins

---
+++ src/mixins/form.mixin.vue
@@ -0,0 +1,28 @@
+<script>
+import { mapGetters } from 'vuex'
+import { extend, ValidationProvider } from 'vee-validate'
+import checkAccent from '@/utils/checkAccent'
+import { isValidCPF } from '@/utils/validations'
+
+extend('validate-accent', {
+  validate: checkAccent,
+  message: 'Não são permitidos acentos'
+})
+
+extend('cpf', {
+  validate: isValidCPF,
+  message: 'CPF inválido'
+})
+
+export default {
+  name: 'form-mixin',
+
+  components: {
+    ValidationProvider
+  },
+
+  computed: {
+    ...mapGetters(['getUser'])
+  }
+}
+</script>
